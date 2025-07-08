50f026b7c5ef46331bf264f3fd58e6efac6dc75d
Ahmed1010 <Ahmed01020238429@gmail.com>
2025-07-08 12:56:52 +0300

    initial commit for this project

---
+++ src/test/java/com/example/DocumrntManagementSystem/DocumrntManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DocumrntManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumrntManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
